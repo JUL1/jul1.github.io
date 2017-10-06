407d7c6ccb5e4b045984315d7c7c4b3d2e476075
jul1 <japrcontact@gmail.com>
2017-10-06 18:33:08 +0200

    ..

---
+++ neon/js/script.js
@@ -0,0 +1,27 @@
+//document.getElementById("togglerBtn").addEventListener("click",toggleMenu);
+var navToggler=document.getElementById("navToggler");
+var navMenu=document.getElementById("navMenu");
+
+function toggleMenu() {
+var visibility = window.getComputedStyle(navToggler, null).getPropertyValue("visibility");
+
+	if(visibility==="visible"){
+		if(navMenu.classList.contains("collapse")) {navMenu.classList.remove("collapse");navMenu.classList.add("deploy");}else{navMenu.classList.add("collapse");navMenu.classList.remove("deploy");};
+	}
+}
+
+function resizeHandler() {
+
+var visibility = window.getComputedStyle(navToggler, null).getPropertyValue("visibility");
+
+	if(visibility!=="visible" && navMenu.classList.contains("deploy")){
+		navMenu.classList.remove("deploy");navMenu.classList.add("collapse");
+	}
+
+}
+
+navToggler.addEventListener("click",toggleMenu);
+navMenu.addEventListener("click",toggleMenu);
+window.addEventListener("resize", resizeHandler);
+
+//toggleMenu();
